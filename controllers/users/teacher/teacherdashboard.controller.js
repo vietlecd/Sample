551a5c8cff86e4fd5de777cd4857f949b360de72
lagomst <133565804+lagomst@users.noreply.github.com>
2024-04-28 09:47:26 +0700

    Delete controllers directory

--- controllers/users/teacher/teacherdashboard.controller.js
+++
@@ -1,9 +0,0 @@
-module.exports.dashboard = (req, res) => {
-    try {
-        // Thực hiện các thao tác thành công ở đây
-        res.send("Xin chào! Đây là dòng tin nhắn thành công của tôi.");
-    } catch (error) {
-        // Xử lý lỗi khi có lỗi xảy ra
-        res.send("Xin lỗi! Đây là dòng tin nhắn thất bại của tôi.");
-    }
-};
--- controllers/users/thongtindt.controller.js
+++
@@ -1,8 +0,0 @@
-const thongDaoTao = require('../../models/teacher.model');
-
-
-module.exports.dashboard = async (req, res) => {
-    const thongtindaotao = await thongDaoTao.find({
-    });
-    console.log( thongtindaotao);
-};
